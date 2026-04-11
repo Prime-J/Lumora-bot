956080c1c6fb100b7f5838cf34779813a18e7faf
Prime-J <romiosavager@gmail.com>
2026-04-11 16:32:38 +0200

    added pairing code support

--- index.js
+++ index.js
@@ -1284,7 +1284,14 @@ async function startBot() {
   });
 
   sock.ev.on("creds.update", saveCreds);
-
+if (!sock.authState.creds.registered) {
+    const phoneNumber = "YOUR_NUMBER_WITH_COUNTRY_CODE"; // e.g., "234..."
+    setTimeout(async () => {
+        let code = await sock.requestPairingCode(phoneNumber);
+        code = code?.match(/.{1,4}/g)?.join("-") || code;
+        console.log(`\n\n=== YOUR PAIRING CODE IS: ${code} ===\n\n`);
+    }, 3000);
+}
   let spawnStarted = false;
 
   sock.ev.on("connection.update", (update) => {
